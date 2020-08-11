c93aca0fde3fa55f93665fde853b9710e912ab42
Fady Soliman <fady.soliman@valocity.co.nz>
2020-08-11 17:54:31 +1200

    adding map choices to the left nav drawer

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import * as L from 'leaflet';
-import { DataService } from "../../services/data-service.service";
+import { DataService } from '../../services/data-service.service';
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
@@ -9,9 +9,7 @@ import { DataService } from "../../services/data-service.service";
 export class MapComponent implements OnInit, AfterViewInit {
   private map;
 
-  constructor(
-    private dataService: DataService
-  ) {}
+  constructor(private dataService: DataService) {}
 
   ngOnInit(): void {}
 
@@ -22,54 +20,65 @@ export class MapComponent implements OnInit, AfterViewInit {
   }
 
   initMap() {
-
     var bed_icon = L.icon({
       iconUrl: 'assets/icons/map-pins/bed/bed_64x64.png',
-      iconSize:     [64, 64], // size of the icon
-      iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
-      popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+      iconSize: [64, 64], // size of the icon
+      iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+      popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
     });
 
     this.map = L.map('map', {
       // Auckland center: [-36.848701, 174.763873]
-      center: [-41.1346502,174.8383448],
+      center: [-41.1346502, 174.8383448],
       zoom: 14,
     });
 
-    const OSM = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+    const OSM = L.tileLayer(
+      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      {
         maxZoom: 19,
-        attribution:'&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-      }
-    );
-  
-    const EsriWorldImagery = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
-        maxZoom: 20, 
-        attribution: '&copy; <a href="https://www.arcgis.com/home/item.html?id=974d45be315c4c87b2ac32be59af9a0b">Esri</a> contributors'
+        attribution:
+          '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }
     );
 
+    const EsriWorldImagery = L.tileLayer(
+      'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+      {
+        maxZoom: 20,
+        attribution:
+          '&copy; <a href="https://www.arcgis.com/home/item.html?id=974d45be315c4c87b2ac32be59af9a0b">Esri</a> contributors',
+      }
+    );
 
     OSM.addTo(this.map);
 
-
     // fetching geojson data
     this.dataService.getProperty().subscribe((data) => {
+      let propertyList = data['features'] as object[];
+      console.log('DATA: ', propertyList);
 
-      console.log('DATA: ', data.features);
+      var Shelters = L.geoJSON(propertyList, {
+        pointToLayer: function (feature, latlng) {
+          return L.marker(latlng, { icon: bed_icon });
+        },
+      })
+        .addTo(this.map)
+        .bindPopup(
+          '<b>' +
+            'Name:' +
+            propertyList['properties'] +
+            '</b><br>' +
+            'Cost: ' +
+            propertyList['properties']
+        );
 
-      var Beds = L.geoJSON(data, {
-        pointToLayer: function (feature, latlng) {return L.marker(latlng, {icon: bed_icon});}
-      }).addTo(this.map)
-      .bindPopup(
-        '<b>' + 
-        'Name:' + data.features + 
-        '</b><br>' + 
-        'Cost: ' + data.features)
-
-        const overlayMaps = {"Beds": Beds};
-        const BaseMaps = {"Open Street Map": OSM,"Esri Imagery": EsriWorldImagery};
-        L.control.layers(BaseMaps, overlayMaps).addTo(this.map);
-
-    })
+      const overlayMaps = { Shelters: Shelters };
+      const BaseMaps = {
+        'Open Street Map': OSM,
+        'Esri Imagery': EsriWorldImagery,
+      };
+      L.control.layers(BaseMaps, overlayMaps).addTo(this.map);
+    });
   }
 }
